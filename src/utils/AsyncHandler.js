41e3f94ae2400b0a21556b2bd291fbd29c6bbf24
Sat2jan <satyamrathore2jan@gmail.com>
2024-12-22 21:04:34 +0530

    utils code discussion

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,29 @@
+// this is Promise methods.....
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+  }
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async ()=> {}
+
+
+
+  //this is a try catch process....
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//       await fn(req, res, next)
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message
+//     })
+//   }
+// } 
